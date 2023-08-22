938c53b80e7d0325f148234bf2018e7dda96176c
Zhiwei Fan <mortalsix@qq.com>
2023-08-22 18:00:09 +0800

    2

--- Lab3/Lab3JavaProject/src/Lab3P01.java
+++ Lab3/Lab3JavaProject/src/Lab3P01.java
@@ -1,4 +1,16 @@
-public class App {
+import java.io.IOException;
+import java.util.List;
+
+public class Lab3P01 {
+    public static void listTables() throws IOException {
+        init();//建立连接
+        List<TableDescriptor> tableDescriptors = admin.listTableDescriptors();
+        for(TableDescriptor tableDescriptor : tableDescriptors){
+            TableName tableName = tableDescriptor.getTableName();
+            System.out.println("Table:" + tableName);
+        }
+        close();//关闭连接
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
