acf8b3eacf567ddb97943f73bc528499486f62e5
Zhiwei Fan <mortalsix@qq.com>
2023-08-23 16:10:58 +0800

    2

--- Lab3/Lab3JavaProject/src/Lab3P01.java
+++ Lab3/Lab3JavaProject/src/Lab3P01.java
@@ -3,13 +3,7 @@
  * 问题 1. 列出 HBase 所有表的相关信息，如表名、创建时间等。
  */
 
-import java.io.IOException;
-import java.util.List;
-
 public class Lab3P01 {
-    public static void listTables() throws IOException {
-        
-    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
