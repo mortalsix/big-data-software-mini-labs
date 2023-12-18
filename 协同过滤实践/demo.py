7f26b0638d357eb0c77ea959e1a1ccb59e2dbe96
mortalsix <mortalsix@qq.com>
2023-12-18 14:09:35 +0800

    2

--- 协同过滤实践/demo.py
+++ 协同过滤实践/demo.py
@@ -1,20 +1,42 @@
 import math 
 import os
+import time
 
+# load data from file and return two dataset: user_movie and movies
 def ReadData():
-    file_user_movie = 'ratings.csv'
-    file_movie_info = 'movies.csv'
-    user_moive = {}
+    file_user_movie = 'ratings.csv'     # userId,movieId,rating,timestamp
+    file_movie_info = 'movies.csv'      # movieId,title,genres
+    user_moive = {}                     # userId: movieId_list
+
     with open(file_user_movie) as f:
         is_firsline = True
         for line in f:
-            if is_firsline:
+            if is_firsline:             # ignore first line 
                 is_firsline = False
                 continue
             user, item, score = line.split(',')[0:3]
             user_moive.setdefault(user, {})
             user_moive[user][item] = float(score)
-    movies = {}
+
+    # user_movie Example:
+    # user_movie = {
+    #     '1' : {
+    #         '1' : 4.0,
+    #         '3' : 4.0,
+    #         '6' : 4.0,
+    #         '47' : 5.0,
+    #         ... 
+    #     },
+    #     '3' : {
+    #         '31' : 0.5,
+    #         '527' : 0.5,
+    #         '1587' : 4.5
+    #         ... 
+    #     }
+    #     ...
+    # }
+    
+    movies = {}   # movieId: title
     with open(file_movie_info, encoding='UTF-8') as f:
         is_firsline = True
         for line in f:
@@ -23,8 +45,19 @@ def ReadData():
                 continue
             movieId, movieTitle = line.split(',')[0:2]
             movies[movieId] = movieTitle
+
+    # movies Example:
+    # movies = {
+    #     '1' : 'Toy Story (1995)',
+    #     '2' : 'Jumanji (1995)',
+    #     '3' : 'Grumpier Old Men (1995)',
+    #     ...
+    # }
+            
     return user_moive, movies
 
+
+# caculate item similarity matrix 
 def ItemSimilarity(user_movie):
     C = {}
     N = {}
@@ -57,9 +90,17 @@ def Recommend(user, user_movie, W, K, N):
     return dict(sorted(rank.items(), key = lambda x : x[1], reverse = True)[0:N])
 
 if __name__ == "__main__":
+    t0 = time.time()
     user_movie, movies = ReadData()
+    t1 = time.time()
+    print("加载数据用时：", (t1 - t0))
     W = ItemSimilarity(user_movie)
+    t2 = time.time()
+    print("计算物品相似度矩阵用时：", (t2 - t1))
     result = Recommend('1', user_movie, W, 10, 10)
+    t3 = time.time()
+    print("推荐用时：", (t3 - t2))
+    print("最终推荐结果：")
     for i, rating in result.items():
         print('film: ', movies[i], ', rating: ', rating)
     
